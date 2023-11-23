8b094022f86eee1d1ae714a4584e91c93b00b361
Emiliano Mesquita <emiliano_mesquita@hotmail.com>
2023-11-23 16:11:28 -0300

    Bump pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = (Path(__file__).parent / "README.md").read_text()
 
 setup(
     name='afipcaeqrdecode',
-    version='0.0.2',
+    version='0.0.3',
     packages=find_packages(),
     install_requires=[
         'Pillow==10.1.0',
