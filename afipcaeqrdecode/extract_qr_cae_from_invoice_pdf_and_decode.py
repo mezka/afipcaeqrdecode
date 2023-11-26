894cde0065f9e28249a0a25a0e1508c6079a498b
Emiliano Mesquita <emiliano_mesquita@hotmail.com>
2023-11-26 19:27:41 -0300

    Replace pyzbar with qreader

--- afipcaeqrdecode/extract_qr_cae_from_invoice_pdf_and_decode.py
+++ afipcaeqrdecode/extract_qr_cae_from_invoice_pdf_and_decode.py
@@ -1,11 +1,8 @@
 import fitz
-import shutil
 from pathlib import Path
-import os
-from io import BytesIO
-from pyzbar.pyzbar import decode, ZBarSymbol
-from PIL import Image
-import os
+import numpy as np
+import cv2
+from qreader import QReader
 import re
 import jwt
  
@@ -15,22 +12,26 @@ def extract_qr_cae_from_invoice_pdf_and_decode(filepath):
 
     pdf_file = fitz.open(filepath)
     image_list = pdf_file[0].get_images()
-    
+    qreader = QReader()
+
     for image_index, img in enumerate(image_list):
+
         xref = img[0]
         base_image = pdf_file.extract_image(xref)
 
         image_bytes = base_image["image"]
         image_ext = base_image["ext"]
 
-        img = Image.open(BytesIO(image_bytes))
+        image_np = np.frombuffer(image_bytes, np.uint8)
+        image_opencv = cv2.imdecode(image_np, cv2.IMREAD_COLOR)
 
-        decoded_list = decode(img)
-
-        if decoded_list:
-            result = re.match('https:\/\/www\.afip\.gob\.ar\/fe\/qr\/\?p=(.*)', bytes.decode(decoded_list[0].data))
+        decoded_tuple = qreader.detect_and_decode(image_opencv)
+        
+        if decoded_tuple:
+            result = re.match('https:\/\/www\.afip\.gob\.ar\/fe\/qr\/\?p=(.*)', decoded_tuple[0])
             jwt_qr_url = result[1]
 
             decoded_token = bytes.decode(jwt.utils.base64url_decode(jwt_qr_url))
-            return decoded_token
+
+            return decoded_token.replace(' ','').replace('\r', '').replace('\n','').replace('\t', '')
         
